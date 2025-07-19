f52f631f6d104cd097add0b7c1b319c4eed6f5d6
Frankienator <jakob_frank@hotmail.com>
2025-07-19 21:00:57 +0200

    Finished chapter 'Body - Nested Models'

--- src/user_guide/06_advanced_request_body.py
+++ src/user_guide/06_advanced_request_body.py
@@ -129,3 +129,131 @@ async def update_anotherItem(
 
 
 # Body - Nested Models
+
+# utilizing pydantic, you can define, validate, document and use arbitrarily deeply nested Models:
+
+class NestedItem(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+    tags: list = []
+
+@app.put("/nestedItems/{item_id}")
+async def update_item(item_id: int, nestedItem: NestedItem):
+    pass
+
+# this example contains a python list, but the elements itself are not type secure.
+# to change this, we must define our list as such:
+
+from typing import Union
+
+class ListItem(BaseModel):
+    name: str
+    description: Union[str, None] = None # this Union notation is a different (better readable) way to implement "str | None" for example
+    price: float
+    tax: Union[float, None] = None
+    tags: list[str] = []
+
+# in this example, our tags list is specifically typed to only accept strings.
+# For this we also had to import the List parameter from typing.
+# Another relevant alternative for List is "set", which doesn't have duplicates and is defined the same as our List:
+tags: set[str] = []
+
+
+# Nested Models
+
+# in pydantic, nothing is stopping us from nesting models.
+# Thinking object oriented, this is very good for us.
+# An example for this would be:
+
+class InnerItem(BaseModel):
+    url: str
+    name: str
+
+class OuterItem(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+    tags: set[str] = set()
+    image: Union[InnerItem, None] = None
+
+# in our OuterItem class, we used an InnerItem typed attribute image.
+# This nesting has no limits, and can be applied freely.
+# An example of the defined body of type OuterItem would look the following way:
+
+# {
+#     "name": "Foo",
+#     "description": "The pretender",
+#     "price": 42.0,
+#     "tax": 3.2,
+#     "tags": ["rock", "metal", "bar"],
+#     "image": {
+#         "url": "http://example.com/baz.jpg",
+#         "name": "The Foo live"
+#     }
+# }
+
+
+# Special Types
+
+# Pydantic provides special types which can be used for validation.
+# Those types usually inherit from str, and provide special functionality.
+# An example would be HttpUrl, which validates the input for a http url string
+
+
+# Attributes with lists of submodels
+
+# Combining the logic of nested models, and list logic, we can also provide lists or sets of submodels.
+# For example:
+
+class OuterItemWithList(BaseModel):
+    name: str
+    description: Union[str, None] = None
+    price: float
+    tax: Union[float, None] = None
+    tags: set[str] = set()
+    image: list[InnerItem] = []
+
+# An example for a valid form of this response body model:
+
+# {
+#     "name": "Foo",
+#     "description": "The pretender",
+#     "price": 42.0,
+#     "tax": 3.2,
+#     "tags": [
+#         "rock",
+#         "metal",
+#         "bar"
+#     ],
+#     "images": [
+#         {
+#             "url": "http://example.com/baz.jpg",
+#             "name": "The Foo live"
+#         },
+#         {
+#             "url": "http://example.com/dave.jpg",
+#             "name": "The Baz"
+#         }
+#     ]
+# }
+
+
+# Bodies of pure lists
+
+# If the top level value of the JSON body you expect is a JSON array (= python list), you can declare the type in the function as follows:
+@app.post("/images/multiple/")
+async def create_multiple_images(images: list[InnerItem]):
+    return images
+
+
+# Similarly, you can declare a body as a dict with keys of some type and values of some other type
+# This way, you don't have to know beforehand, what valid field/attribute names are (in contrast to pydantic)
+# this is useful if you want to get keys you don't already know about.
+# for example:
+
+@app.post("/index-weights/")
+async def create_index_weights(weights: dict[int, float]):
+    return weights
